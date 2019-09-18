b387d4b6559c2c6a0331f70d5ccd6e4a1011aeb3
Job Weeda <job.weeda@sentia.com>
2019-09-18 08:38:42 +0200

    pydcostyle

--- models/user.py
+++ models/user.py
@@ -1,32 +1,27 @@
-""" User module
-"""
+"""User module."""
 class User:
-    """User class
-    """
+    """User class."""
+
     def __init__(self, name, user_id):
-        """comntruct
-        """
+        """Construct."""
         self._user_id = user_id
         self._name = name
 
 
     def get_user_id(self):
-        """this method returns the user id
-        """
+        """Return the user ID."""
         return self._user_id
 
     def get_name(self):
-        """this method returns name
-        """
+        """Return the name."""
         return self._name
 
     def set_name(self, name):
-        """[summary]
-        """
+        """Set a name."""
         self._name = name
 
     def to_dict(self):
-        """[summary]. """
+        """Return a dict of User."""
         return {
             "user_id": self.get_user_id(),
             "name": self.get_name()
