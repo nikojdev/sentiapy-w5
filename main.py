17e3ca61c40ec3b8c26234776f0d07e611b70d73
Niko J <niko.jernejsek@gmail.com>
2019-09-18 13:58:02 +0200

    error handling added for all exceptions

--- main.py
+++ main.py
@@ -6,16 +6,19 @@ from controllers.user_controller import UserController
 
 app = Flask(__name__)  # pylint: disable=invalid-name
 
+
 @app.route("/")
 def home():
     """Return HW on the  root  path."""
     return "Welcome to the pool."
+
 
 @app.route("/ticket")
 def get_ticket_list():
     """Get a list of all tickets."""
     tickets_array = TicketController.get_tickets()
     return jsonify(tickets_array)
+
 
 @app.route("/ticket", methods=["POST"])
 def create_ticket():
@@ -24,11 +27,13 @@ def create_ticket():
     ticket = TicketController.create_ticket(data)
     return jsonify(ticket.ticket_to_dict())
 
+
 @app.route("/ticket/<int:ticket_id>")
 def get_single_ticket(ticket_id):
     """Return a single ticket based on its ID."""
     ticket = TicketController.get_ticket_by_id(ticket_id)
     return jsonify(ticket.ticket_to_dict())
+
 
 @app.route("/ticket/<int:ticket_id>", methods=["PUT"])
 def update_ticket(ticket_id):
@@ -37,22 +42,24 @@ def update_ticket(ticket_id):
     user_by_id = None
     response = None
 
-    #get ticket by id
+    # get ticket by id
     ticket_by_id = TicketController.get_ticket_by_id(ticket_id)
 
-    #check if there is assignee key provided in request
+    # check if there is assignee key provided in request
     if "ticket_assignee" in ticket_data:
         user_by_id = UserController.get_users_by_id(ticket_data["ticket_assignee"])
         if user_by_id is None:
-            return resource_not_found_response() #return not found response if wrong user provided
+            return (
+                error_response(404, "Resource Not Found")
+            )  # return not found response if wrong user provided
 
-    #check if ticket was found and update accordingly
+    # check if ticket was found and update accordingly
     if ticket_by_id:
         TicketController.update_ticket(ticket_by_id, ticket_data, user_by_id)
         response = success_response_body(ticket_data)
     else:
-        #return no user error
-        response = resource_not_found_response()
+        # return no user error
+        response = error_response(404, "Resource Not Found")
     return response
 
 
@@ -63,14 +70,16 @@ def delete_ticket(ticket_id):
         TicketController.delete_ticket(ticket_id)
         response = success_response_body({})
     else:
-        response = resource_not_found_response()
+        response = error_response(404, "Resource Not Found")
     return response
+
 
 @app.route("/user")
 def get_user_list():
     """Return a list of all users."""
     users_array = UserController.get_users()
     return jsonify(users_array)
+
 
 @app.route("/user", methods=["POST"])
 def create_user():
@@ -79,11 +88,13 @@ def create_user():
     user = UserController.create_user(data)
     return jsonify(user.to_dict())
 
+
 @app.route("/user/<int:user_id>")
 def get_single_user(user_id):
     """Return a specific user based on ID."""
     user = UserController.get_users_by_id(user_id)
     return jsonify(user.to_dict())
+
 
 @app.route("/user/<int:user_id>", methods=["PUT"])
 def update_user(user_id):
@@ -91,15 +102,15 @@ def update_user(user_id):
     user_data = request.get_json()
     response = None
 
-    #get user by id
+    # get user by id
     user_by_id = UserController.get_users_by_id(user_id)
 
     if user_by_id:
         UserController.update_user(user_by_id, user_data)
         response = success_response_body(user_data)
     else:
-        #return no user error
-        response = resource_not_found_response()
+        # return no user error
+        response = error_response(404, "Resource Not Found")
 
     return response
 
@@ -111,21 +122,21 @@ def delete_user(user_id):
     UserController.delete_user(user_id)
     return jsonify({})
 
+
 def success_response_body(data):
     """Return a successful message."""
-    return {
-        "result": True,
-        "data": data
-    }
+    return {"result": True, "data": data}
 
-def resource_not_found_response(message=None):
+
+def error_response(status_code, error, message=None):
     """Return an error message."""
-    body_dict = {
-        "code":4040,
-        "error":"resource not found",
-        "result":False
-    }
+    body_dict = {"code": status_code, "error": error, "result": False}
     if message:
         body_dict["message"] = message
 
-    return jsonify(body_dict), 404
+    return jsonify(body_dict), status_code
+
+@app.errorhandler(Exception)
+def page_not_found(error):
+    """page not found error"""
+    return error_response(error.code, error.description)
